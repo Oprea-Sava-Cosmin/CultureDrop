8ee3611ed0ed144a0f5c7d82bd33b41282ffd06d
Tudor230 <133231902+Tudor230@users.noreply.github.com>
2025-04-06 09:44:45 +0300

    Change env variable

--- client/src/store/appStore.ts
+++ client/src/store/appStore.ts
@@ -1,7 +1,7 @@
 import { Store } from '@tanstack/react-store';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_DATABASE_URL;
 console.log('API_URL:', API_URL);
 
 export interface LoginCredentials {
